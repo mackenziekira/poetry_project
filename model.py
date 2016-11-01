55d43d3b6e1aae5f6ad959dc14f9a6956673344c
Hackbright Student <no-reply@hackbrightacademy.com>
2016-11-01 16:10:02 -0700

    made model.py

--- model.py
+++ model.py
@@ -10,5 +10,92 @@ class Poem(db.Model):
     __tablename__ = 'poems'
 
     poem_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String(200))
-    author_id = db.Column(db.ForeignKey)
+    title = db.Column(db.String(200), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.author_id'))
+    poem_url = db.Column(db.String(300), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+    author = db.relationship('Author', backref='poems')
+    subjects = db.relationship('Subject', secondary='poems_subjects', backref='poems')
+    poetic_terms = db.relationship('PoeticTerm', secondary='poems_poetic_terms', backref='poems')
+
+class Author(db.Model):
+    """class for author objects"""
+
+    __tablename__ = 'authors'
+
+    author_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    region_code = db.Column(db.String(100), db.ForeignKey('regions.region_code'))
+    affiliation_code = db.Column(db.String(100), db.ForeignKey('affiliation.affiliation_code'))
+    birthdate = db.Column(db.Date)
+    deathdate = db.Column(db.Date)
+    bio = db.Column(db.Text)
+
+    region = db.relationship('Region', backref='authors')
+    affiliation = db.relationship('Affiliation', backref='authors')
+
+class Region(db.Model):
+    """class for region objects"""
+
+    __tablename__ = 'regions'
+
+    region_code = db.Column(db.String(100), primary_key=True)
+    region_name = db.Column(db.String(300), nullable=False)
+
+class Affiliation(db.Model):
+    """class for affiliation objects"""
+
+    __tablename__ = 'affiliations'
+
+    affiliation_code = db.Column(db.String(100), primary_key=True)
+    affiliation_name = db.Column(db.String(300), nullable=False)
+
+class PoemPoeticTerm(db.Model):
+    """association table connecting poems with poetic terms"""
+
+    __tablename__ = 'poems_poetic_terms'
+
+    poems_poetic_terms_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    term_code = db.Column(db.String(100), db.ForeignKey('poetic_terms.term_code'), nullable=False)
+    poem_id = db.Column(db.Integer, db.ForeignKey('poems.poem_id'), nullable=False)
+
+class PoemSubject(db.Model):
+    """association table connecting poems with subjects"""
+
+    __tablename__ = 'poems_subjects'
+
+    poems_subjects_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    subject_code = db.Column(db.String(100), db.ForeignKey('subjects.term_code'), nullable=False)
+    poem_id = db.Column(db.Integer, db.ForeignKey('poems.poem_id'), nullable=False)
+
+class PoeticTerm(db.Model):
+    """class for poetic terms associated with a poem"""
+
+    __tablename__ = 'poetic_terms'
+
+    term_code = db.Column(db.String(100), primary_key=True)
+    term_name = db.Column(db.String(300), nullable=False)
+
+class Subject(db.Model):
+    """class for subjects"""
+
+    __tablename__ = 'subjects'
+
+    subject_code = db.Column(db.String(100), primary_key=True)
+    subject_name = db.Column(db.String(300), nullable=False)
+
+
+
+
+def connect_to_db(app):
+    """Connect the database to Flask application"""
+
+    app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql:///poetry'
+    db.app = app
+    db.init_app(app)
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
+    print 'connected to db'
---
+++ server.py
@@ -0,0 +1,4 @@
+from flask import flask
+from model import connect_to_db, db
+
+app = Flask(__name__)
