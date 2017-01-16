98512b77cd0ca7c44cba0cc21767d5a6a18f9d5d
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-16 14:49:47 -0800

    added temporary fix to search issue, alerting user if they try to enter more than one search word

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -13,8 +13,19 @@ function highlightText(evt) {
   $(subj).toggleClass("highlight");
 }
 
+function checkNoSpace(evt) {
+  var textEntered = this.value;
+  if (textEntered.indexOf(' ') > -1) {
+    $('#error').html('Search word must be one word.');
+  }
+  else {
+    $('#error').html('');
+  }
+}
+
 
 $('.headline').on('click', toggleText)
 $('li').on('click', highlightText)
 $('.poembody').mark($('div[term]').attr('id'))
+$('input').on('keyup', checkNoSpace);
 
